5d4b85aedc03bae63968d83afe2b2d7536c77bb1
arshia <arshiarezagholi1212@gmail.com>
2024-08-20 23:42:54 +0330

    added doc strings for the user class

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,16 @@ from django.db import models
 
 
 class User(AbstractUser):
+    """
+    Custom User model extending the default Django AbstractUser.
+
+    Attributes:
+        email (EmailField): The user's email address, which must be unique.
+        phone_number (CharField): The user's phone number, which is optional and must be unique if provided.
+        is_active (BooleanField): Indicates whether the user account is active.
+         (Based on if the user confirmed his email)
+    """
+
     email = models.EmailField(
         unique=True,
     )
