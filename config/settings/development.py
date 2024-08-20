0979753bb70bc7d18c8bcbbc555cfb37b3e292a9
arshia <arshiarezagholi1212@gmail.com>
2024-08-20 19:42:54 +0330

    removed comments in development settings

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,14 +1,10 @@
 from .base import *
 
-# Debug mode. When set to True, Django will display a detailed error page whenever an exception is raised.
 DEBUG = True
 
-# List of allowed hosts for this site.
 # '*' means all hosts are allowed.
 ALLOWED_HOSTS = ["*"]
 
-# Database configuration.
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
     "default": {
         # The database engine to use.
