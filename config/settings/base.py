b65980c9f033d86d5cd954b597cf7e07eda96431
arshia <arshiarezagholi1212@gmail.com>
2024-08-20 19:48:20 +0330

    added accounts app

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,6 +16,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third party apps
+    "rest_framework",
+    "django_extensions",
+    # local apps
+    "accounts",
 ]
 
 MIDDLEWARE = [
