bc24f4d3e8372266bec93a58d09e65142270f1ad
arshia <arshiarezagholi1212@gmail.com>
2024-08-21 11:20:49 +0330

    added UserRegister View

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from accounts.serializers import UserRegisterSerializer
+
+
+class UserRegisterView(generics.CreateAPIView):
+    serializer_class = UserRegisterSerializer
+    permission_classes = (AllowAny,)
