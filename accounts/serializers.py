fe6bc57fbd0b500a96ac9d4a888ca2d9fbdf0a2f
arshia <arshiarezagholi1212@gmail.com>
2024-08-21 16:20:05 +0330

    added doc strings for register serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,6 +3,26 @@ from .models import User
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
+    """
+    Serializer for registering a new user.
+
+    This serializer handles the creation of a new user by taking in the necessary
+    fields and creating a user instance with the provided data.
+
+    Fields:
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+        email (str): The email address of the user.
+        username (str): The username of the user.
+        password (str): The password for the user account.
+        phone_number (str): The phone number of the user.
+
+    Methods:
+        create(validated_data):
+            Creates and returns a new user instance with the validated data.
+            (override to make sure the password will be hashed)
+    """
+
     class Meta:
         model = User
         fields = [
