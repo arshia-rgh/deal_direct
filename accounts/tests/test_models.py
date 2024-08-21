bc88207c9ad11413e9e46b21b283e97a12b389cb
arshia <arshiarezagholi1212@gmail.com>
2024-08-21 14:05:30 +0330

    added tests for user model

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,6 +1,18 @@
+from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase
+
+User = get_user_model()
 
 
 class UserModelAPITestCase(APITestCase):
     def setUp(self):
-        pass
+        self.user1 = User.objects.create_user(
+            username="testuser",
+            email="test@gmail.com",
+            password="testpassword12",
+        )
+
+    def test_user_creation(self):
+        self.assertEqual(self.user1.username, "testuser")
+        self.assertTrue(User.objects.filter(username="testuser").exists())
+        self.assertTrue(self.user1.check_password("testpassword12"))
