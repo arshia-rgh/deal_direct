8326acde87084dae138aaed12471f1bfb4a37bad
arshia <arshiarezagholi1212@gmail.com>
2024-08-21 12:26:05 +0330

    completed the celery task

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,4 +1,7 @@
 from celery import shared_task
+from django.conf import settings
+from django.core.mail import send_mail
+from django.urls import reverse
 
 from utils.email_verification_generator import (
     generate_email_verification_token,
@@ -14,3 +17,9 @@ def send_email_verification_link(user_id):
     user = User.objects.get(user_id)
     token = generate_email_verification_token(user)
     uid = generate_uid(user)
+
+    verification_link = f"{settings.BASE_URL}{reverse('accounts:verify_email', kwargs={'uidb64': uid, 'token': token})}"
+
+    subject = "Verify your email address"
+    message = f"Please click the link below to verify your email address:\n{verification_link}"
+    send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email])
