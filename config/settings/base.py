34bd7ed1294b151aba085f5f5da968956aabd3b2
arshia <arshiarezagholi1212@gmail.com>
2024-08-21 15:39:01 +0330

    added auth backends

--- config/settings/base.py
+++ config/settings/base.py
@@ -86,6 +86,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 AUTH_USER_MODEL = "accounts.User"
 
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
