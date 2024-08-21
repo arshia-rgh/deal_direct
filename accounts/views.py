8150143f6f0a94c9009df0afc1c5bae75ec2da19
arshia <arshiarezagholi1212@gmail.com>
2024-08-21 12:19:00 +0330

    added VerifyEmailView

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.shortcuts import get_object_or_404
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
 from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from accounts.models import User
 from accounts.serializers import UserRegisterSerializer
 
 
@@ -17,3 +23,39 @@ class UserRegisterView(generics.CreateAPIView):
             },
             status=status.HTTP_201_CREATED,
         )
+
+
+class VerifyEmailView(APIView):
+    """
+    View that handles email verification
+    """
+
+    def get(self, request, uidb64, token, *args, **kwargs):
+        """
+        Verifies the user's email.
+
+        Args:
+            request (Request): The DRF request object.
+            uidb64 (str): The base64 encoded user ID.
+            token (str): The token for email verification.
+
+        Returns:
+            Response: A response object containing a success or failure message.
+        """
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = get_object_or_404(User, pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+            return Response(
+                {"message": "Email verified successfully."}, status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {"message": "Invalid verification link."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
