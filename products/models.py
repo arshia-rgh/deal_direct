ff1982759c7d73a46118314aeb327d2ed59d3049
arshia <arshiarezagholi1212@gmail.com>
2024-08-22 03:09:47 +0330

    completed Product and Category models

--- products/models.py
+++ products/models.py
@@ -1,9 +1,15 @@
 from utils.base_model import BaseModel
+from django.db import models
 
 
 class Product(BaseModel):
-    pass
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to="img/products/")
+    category = models.ForeignKey(to="Category", on_delete=models.CASCADE)
 
 
 class Category(BaseModel):
-    pass
+    name = models.CharField(max_length=100)
+    description = models.TextField()
