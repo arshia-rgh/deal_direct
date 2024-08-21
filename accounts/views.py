7b06ab59fdfec98bb478d72c1d5d5e85e6261f21
arshia <arshiarezagholi1212@gmail.com>
2024-08-21 13:29:50 +0330

    added UserLoginView

--- accounts/views.py
+++ accounts/views.py
@@ -78,3 +78,7 @@ class VerifyEmailView(APIView):
                 {"message": "Invalid verification link."},
                 status=status.HTTP_400_BAD_REQUEST,
             )
+
+
+class UserLoginView:
+    pass
