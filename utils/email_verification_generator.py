f17c14c0b32c34531b2295c32a525978a806f891
arshia <arshiarezagholi1212@gmail.com>
2024-08-21 11:38:08 +0330

    added email verification generator utils

---
+++ utils/email_verification_generator.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
+
+def generate_email_verification_token(user):
+    return default_token_generator(user)
+
+
+def generate_uid(user):
+    return urlsafe_base64_encode(force_bytes(user.pk))
