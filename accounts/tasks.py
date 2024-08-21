a3b5cbffb94867d0a631dd40e606f43a11786b25
arshia <arshiarezagholi1212@gmail.com>
2024-08-21 12:11:20 +0330

    send mail celery task

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,6 +1,16 @@
 from celery import shared_task
 
+from utils.email_verification_generator import (
+    generate_email_verification_token,
+    generate_uid,
+)
+
 
 @shared_task
 def send_email_verification_link(user_id):
-    pass
+    from django.contrib.auth import get_user_model
+
+    User = get_user_model()
+    user = User.objects.get(user_id)
+    token = generate_email_verification_token(user)
+    uid = generate_uid(user)
