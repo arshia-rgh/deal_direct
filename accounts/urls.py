1463b7ce345decac9e90010531c65008b1c7484c
arshia <arshiarezagholi1212@gmail.com>
2024-08-21 12:21:55 +0330

    added verify email to the urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import UserRegisterView
+from .views import UserRegisterView, VerifyEmailView
 
 app_name = "accounts"
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
+    path(
+        "verify-email/<uidb64>/<token>/", VerifyEmailView.as_view(), name="verify_email"
+    ),
 ]
