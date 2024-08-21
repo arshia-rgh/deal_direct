98573ae0d4c01e4de4f130c599146748ff9b80de
arshia <arshiarezagholi1212@gmail.com>
2024-08-21 14:25:15 +0330

    added tests for UserRegisterViewAPITestCase

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,5 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
 from rest_framework.test import APITestCase
+
+User = get_user_model()
 
 
 class UserRegisterViewAPITestCase(APITestCase):
-    pass
+    def test_register(self):
+        response = self.client.post(
+            reverse("accounts:register"),
+            data={
+                "username": "testuser",
+                "password": "test12pass",
+                "email": "test@email.com",
+            },
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(
+            response.data,
+            {
+                "message": "User registered successfully, please check your email to active your account"
+            },
+        )
+        self.assertTrue(User.objects.filter(username="testuser").exists())
