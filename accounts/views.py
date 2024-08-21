bb8e94a05ef04f406196eb9bdcd663c7226a371e
arshia <arshiarezagholi1212@gmail.com>
2024-08-21 12:15:01 +0330

    over ride the create in UserRegister View for sending message to check the email

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from accounts.serializers import UserRegisterSerializer
 
@@ -7,3 +8,12 @@ from accounts.serializers import UserRegisterSerializer
 class UserRegisterView(generics.CreateAPIView):
     serializer_class = UserRegisterSerializer
     permission_classes = (AllowAny,)
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response(
+            {
+                "message": "User registered successfully, please check your email to active your account"
+            },
+            status=status.HTTP_201_CREATED,
+        )
