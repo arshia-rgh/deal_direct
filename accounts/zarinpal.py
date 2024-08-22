ec44a78e8b7f1d065588be55ba0753a11979f29b
arshia <arshiarezagholi1212@gmail.com>
2024-08-22 13:10:45 +0330

    added zarinpal file

---
+++ accounts/zarinpal.py
@@ -0,0 +1,69 @@
+import json
+
+import requests
+from django.conf import settings
+from django.urls import reverse_lazy
+
+if settings.ZARINPAL["SANDBOX"]:
+    sandbox = "sandbox"
+else:
+    sandbox = "www"
+
+ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
+ZP_API_VERIFY = (
+    f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
+)
+ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
+
+
+def send_request(request, amount, description, phone, email):
+    data = {
+        "MerchantID": settings.ZARINPAL["MERCHANT"],
+        "Amount": amount,
+        "Description": description,
+        "Phone": phone,
+        "Email": email,
+        "CallbackURL": ...,
+    }
+    data = json.dumps(data)
+    # set content length by data
+    headers = {"content-type": "application/json", "content-length": str(len(data))}
+    try:
+        response = requests.post(ZP_API_REQUEST, data=data, headers=headers, timeout=10)
+
+        if response.status_code == 200:
+            response = response.json()
+            if response["Status"] == 100:
+                return {
+                    "status": True,
+                    "url": ZP_API_STARTPAY + str(response["Authority"]),
+                    "authority": response["Authority"],
+                }
+            else:
+                return {"status": False, "code": str(response["Status"])}
+        return response
+
+    except requests.exceptions.Timeout:
+        return {"status": False, "code": "timeout"}
+    except requests.exceptions.ConnectionError:
+        return {"status": False, "code": "connection error"}
+
+
+def verify(amount, authority):
+    data = {
+        "MerchantID": settings.ZARINPAL["MERCHANT"],
+        "Amount": amount,
+        "Authority": authority,
+    }
+    data = json.dumps(data)
+    # set content length by data
+    headers = {"content-type": "application/json", "content-length": str(len(data))}
+    response = requests.post(ZP_API_VERIFY, data=data, headers=headers)
+
+    if response.status_code == 200:
+        response = response.json()
+        if response["Status"] == 100:
+            return {"status": True, "RefID": response["RefID"]}
+        else:
+            return {"status": False, "code": str(response["Status"])}
+    return response
