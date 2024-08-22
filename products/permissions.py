700f7e5fc070d1920f852e205aed834082b8f26f
arshia <arshiarezagholi1212@gmail.com>
2024-08-22 04:16:31 +0330

    added some comments for my custom permission

--- products/permissions.py
+++ products/permissions.py
@@ -3,8 +3,12 @@ from rest_framework.permissions import BasePermission
 
 class IsOwnerOrReadOnly(BasePermission):
     def has_permission(self, request, view):
+        # Allow any user to view the list or retrieve a product
+
         if view.action in ["list", "retrieve"]:
             return True
+
+        # Allow authenticated users to create a product
 
         if view.action == "create":
             return (
@@ -15,8 +19,12 @@ class IsOwnerOrReadOnly(BasePermission):
         return False
 
     def has_object_permission(self, request, view, obj):
+        # Allow any user to view the product details
+
         if view.action in ["retrieve"]:
             return True
+
+        # Allow only the owner to update or delete the product
 
         if view.action in ["update", "partial_update", "destroy"]:
             return obj.uploaded_by == request.user
