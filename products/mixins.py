863e7b0e288f8889b2515ea1323af131a4065857
arshia <arshiarezagholi1212@gmail.com>
2024-08-22 12:35:28 +0330

    added throttle mixin

--- products/mixins.py
+++ products/mixins.py
@@ -1,5 +1,6 @@
 from django.core.cache import cache
 from rest_framework.response import Response
+from rest_framework.throttling import ScopedRateThrottle
 
 
 class ListCacheMixin:
@@ -18,3 +19,13 @@ class ListCacheMixin:
 
         serializer = self.get_serializer(cached_queryset, many=True)
         return Response(serializer.data)
+
+
+class ThrottleMixin:
+    def get_throttles(self):
+        if self.request.method in ["PATCH", "PUT", "POST"]:
+            self.throttle_scope = "uploads"
+
+        else:
+            self.throttle_scope = "receives"
+        return [ScopedRateThrottle()]
