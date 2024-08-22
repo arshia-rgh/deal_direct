f9820dcf0822a5445b82984bf6f4274a8b454dbd
arshia <arshiarezagholi1212@gmail.com>
2024-08-22 16:35:29 +0330

    made tests cbv

--- accounts/tests/test_auth_views.py
+++ accounts/tests/test_auth_views.py
@@ -11,133 +11,133 @@ from accounts.tasks import send_email_verification_link
 User = get_user_model()
 
 
-@pytest.mark.django_db
-def test_register_correct_data(api_client):
-    response = api_client.post(
-        reverse("accounts:register"),
-        data={
-            "username": "testuser",
-            "password": "test12pass",
-            "email": "test@email.com",
-        },
-    )
-    assert response.status_code == 201
-    assert response.data == {
-        "message": "User registered successfully, please check your email to active your account"
-    }
-    assert User.objects.filter(username="testuser").exists()
-    assert not User.objects.get(username="testuser").is_active
-
-
-@pytest.mark.django_db
-def test_register_missing_fields(api_client):
-    response = api_client.post(
-        reverse("accounts:register"),
-        data={"username": "testuser"},
-    )
-    assert response.status_code == 400
-    assert "password" in response.data
-    assert "email" in response.data
-
-
-@pytest.mark.django_db
-def test_register_existing_username(api_client):
-    User.objects.create_user(
-        username="testuser", email="test1@email.com", password="test12pass"
-    )
-    response = api_client.post(
-        reverse("accounts:register"),
-        data={
-            "username": "testuser",
-            "password": "test12pass",
-            "email": "test2@email.com",
-        },
-    )
-    assert response.status_code == 400
-    assert "username" in response.data
-
-
-@pytest.mark.django_db
-def test_register_invalid_email(api_client):
-    response = api_client.post(
-        reverse("accounts:register"),
-        data={
-            "username": "testuser",
-            "password": "test12pass",
-            "email": "invalid-email",
-        },
-    )
-    assert response.status_code == 400
-    assert "email" in response.data
-
-
-@pytest.mark.django_db
-def test_email_verification_success(api_client, inactive_user, email_verification_data):
-    uid, token = email_verification_data
-    response = api_client.get(
-        reverse(
-            "accounts:verify_email",
-            kwargs={"uidb64": uid, "token": token},
+class TestUserRegisterView:
+    @pytest.mark.django_db
+    def test_register_correct_data(api_client):
+        response = api_client.post(
+            reverse("accounts:register"),
+            data={
+                "username": "testuser",
+                "password": "test12pass",
+                "email": "test@email.com",
+            },
         )
-    )
-    assert response.status_code == 200
-    assert response.data == {
-        "message": "Email verified successfully. A bonus will be added to your wallet soon"
-    }
-    inactive_user.refresh_from_db()
-    assert inactive_user.is_active
+        assert response.status_code == 201
+        assert response.data == {
+            "message": "User registered successfully, please check your email to active your account"
+        }
+        assert User.objects.filter(username="testuser").exists()
+        assert not User.objects.get(username="testuser").is_active
 
-
-@pytest.mark.django_db
-def test_email_verification_invalid_token(
-    api_client, inactive_user, email_verification_data
-):
-    uid, _ = email_verification_data
-    response = api_client.get(
-        reverse(
-            "accounts:verify_email",
-            kwargs={"uidb64": uid, "token": "invalid-token"},
+    @pytest.mark.django_db
+    def test_register_missing_fields(api_client):
+        response = api_client.post(
+            reverse("accounts:register"),
+            data={"username": "testuser"},
         )
-    )
-    assert response.status_code == 400
-    assert response.data == {"message": "Invalid verification link."}
-    inactive_user.refresh_from_db()
-    assert not inactive_user.is_active
+        assert response.status_code == 400
+        assert "password" in response.data
+        assert "email" in response.data
 
-
-@pytest.mark.django_db
-def test_email_verification_invalid_uid(api_client, email_verification_data):
-    _, token = email_verification_data
-    invalid_uid = urlsafe_base64_encode(force_bytes(999))
-    response = api_client.get(
-        reverse(
-            "accounts:verify_email",
-            kwargs={"uidb64": invalid_uid, "token": token},
+    @pytest.mark.django_db
+    def test_register_existing_username(api_client):
+        User.objects.create_user(
+            username="testuser", email="test1@email.com", password="test12pass"
         )
-    )
-    assert response.status_code == 404
+        response = api_client.post(
+            reverse("accounts:register"),
+            data={
+                "username": "testuser",
+                "password": "test12pass",
+                "email": "test2@email.com",
+            },
+        )
+        assert response.status_code == 400
+        assert "username" in response.data
+
+    @pytest.mark.django_db
+    def test_register_invalid_email(api_client):
+        response = api_client.post(
+            reverse("accounts:register"),
+            data={
+                "username": "testuser",
+                "password": "test12pass",
+                "email": "invalid-email",
+            },
+        )
+        assert response.status_code == 400
+        assert "email" in response.data
 
 
-@pytest.mark.django_db
-@override_settings(EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend")
-def test_send_mail(api_client):
-    response = api_client.post(
-        reverse("accounts:register"),
-        data={
-            "username": "testuser",
-            "password": "test12pass",
-            "email": "test@email.com",
-        },
-    )
-    assert response.status_code == 201
+class TestVerifyEmailView:
+    @pytest.mark.django_db
+    def test_email_verification_success(
+        api_client, inactive_user, email_verification_data
+    ):
+        uid, token = email_verification_data
+        response = api_client.get(
+            reverse(
+                "accounts:verify_email",
+                kwargs={"uidb64": uid, "token": token},
+            )
+        )
+        assert response.status_code == 200
+        assert response.data == {
+            "message": "Email verified successfully. A bonus will be added to your wallet soon"
+        }
+        inactive_user.refresh_from_db()
+        assert inactive_user.is_active
 
-    user = User.objects.get(username="testuser")
-    send_email_verification_link(user.id)
+    @pytest.mark.django_db
+    def test_email_verification_invalid_token(
+        api_client, inactive_user, email_verification_data
+    ):
+        uid, _ = email_verification_data
+        response = api_client.get(
+            reverse(
+                "accounts:verify_email",
+                kwargs={"uidb64": uid, "token": "invalid-token"},
+            )
+        )
+        assert response.status_code == 400
+        assert response.data == {"message": "Invalid verification link."}
+        inactive_user.refresh_from_db()
+        assert not inactive_user.is_active
 
-    assert len(mail.outbox) == 1
-    assert mail.outbox[0].subject == "Verify your email address"
-    assert (
-        "Please click the link below to verify your email address:"
-        in mail.outbox[0].body
-    )
-    assert mail.outbox[0].to == ["test@email.com"]
+    @pytest.mark.django_db
+    def test_email_verification_invalid_uid(api_client, email_verification_data):
+        _, token = email_verification_data
+        invalid_uid = urlsafe_base64_encode(force_bytes(999))
+        response = api_client.get(
+            reverse(
+                "accounts:verify_email",
+                kwargs={"uidb64": invalid_uid, "token": token},
+            )
+        )
+        assert response.status_code == 404
+
+
+class TestSendMail:
+    @pytest.mark.django_db
+    @override_settings(EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend")
+    def test_send_mail(api_client):
+        response = api_client.post(
+            reverse("accounts:register"),
+            data={
+                "username": "testuser",
+                "password": "test12pass",
+                "email": "test@email.com",
+            },
+        )
+        assert response.status_code == 201
+
+        user = User.objects.get(username="testuser")
+        send_email_verification_link(user.id)
+
+        assert len(mail.outbox) == 1
+        assert mail.outbox[0].subject == "Verify your email address"
+        assert (
+            "Please click the link below to verify your email address:"
+            in mail.outbox[0].body
+        )
+        assert mail.outbox[0].to == ["test@email.com"]
