264c3f80b35d724d1f4e5d5434ef0a6c4762cae0
arshia <arshiarezagholi1212@gmail.com>
2024-08-22 16:32:02 +0330

    made test_models class based

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -4,24 +4,22 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-def test_user_creation(test_user):
-    assert test_user.username == "testuser"
-    assert User.objects.filter(username="testuser").exists()
-    assert test_user.check_password("testpassword12")
+class TestUserModel:
+    def test_user_creation(self, test_user):
+        assert test_user.username == "testuser"
+        assert User.objects.filter(username="testuser").exists()
+        assert test_user.check_password("testpassword12")
 
+    def test_user_creation_invalid_data(self, db):
+        with pytest.raises(ValueError):
+            User.objects.create_user(username="", email="invalid email", password="")
 
-def test_user_creation_invalid_data(db):
-    with pytest.raises(ValueError):
-        User.objects.create_user(username="", email="invalid email", password="")
+    def test_user_update(self, test_user):
+        test_user.username = "updateduser"
+        test_user.save()
+        assert test_user.username == "updateduser"
 
-
-def test_user_update(test_user):
-    test_user.username = "updateduser"
-    test_user.save()
-    assert test_user.username == "updateduser"
-
-
-def test_user_deletion(test_user):
-    user_id = test_user.id
-    test_user.delete()
-    assert not User.objects.filter(id=user_id).exists()
+    def test_user_deletion(self, test_user):
+        user_id = test_user.id
+        test_user.delete()
+        assert not User.objects.filter(id=user_id).exists()
