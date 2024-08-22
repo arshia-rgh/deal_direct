75572a180f8d564cbf7ca067798f53dac7da3c4d
arshia <arshiarezagholi1212@gmail.com>
2024-08-22 11:48:43 +0330

    added docstrings

--- products/views.py
+++ products/views.py
@@ -7,16 +7,28 @@ from .permissions import IsOwnerOrReadOnly
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing, creating, updating, and deleting products.
+
+    - Any user (even anonymous) can view the list of products or retrieve a product.
+    - Only authenticated users can create a product.
+    - Only the user who uploaded the product can update or delete it.
+    """
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    # Any user (even anonymous) can view the list of products or retrieve a product.
-    # Only authenticated users can create a product.
-    # Only the user who uploaded the product can update or delete it.
     permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing, creating, updating, and deleting categories.
+
+    - Any user (even anonymous) can view the list of categories or retrieve a category.
+    - Only authenticated users can create, update, or delete a category.
+    """
+
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
