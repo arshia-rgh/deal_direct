b940f5052a0bb49ad031c1fb8649cc735e8d2d9d
arshia <arshiarezagholi1212@gmail.com>
2024-08-22 04:17:34 +0330

    added 2 permissions to the productvIEWSET

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from products.models import Product
 from products.serializers import ProductSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -11,4 +13,4 @@ class ProductViewSet(viewsets.ModelViewSet):
     # Any user (even anonymous) can view the list of products or retrieve a product.
     # Only authenticated users can create a product.
     # Only the user who uploaded the product can update or delete it.
-    permission_classes = []
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
