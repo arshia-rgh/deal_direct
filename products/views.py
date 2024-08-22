8c55a6f00d3b652c452d9ac95752c6096d335c02
arshia <arshiarezagholi1212@gmail.com>
2024-08-22 04:05:26 +0330

    completed ProductViewSet

--- products/views.py
+++ products/views.py
@@ -1,5 +1,9 @@
 from rest_framework import viewsets
 
+from products.models import Product
+from products.serializers import ProductSerializer
+
 
 class ProductViewSet(viewsets.ModelViewSet):
-    pass
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
