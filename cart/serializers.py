a8eea4530f2f91ca56d7735a82789f3cb9bacfad
arshia <arshiarezagholi1212@gmail.com>
2024-08-23 18:16:29 +0330

    added CartItem serializer

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from cart.models import Cart
+from cart.models import Cart, CartItem
 
 
 class CartSerializer(serializers.ModelSerializer):
@@ -14,3 +14,14 @@ class CartSerializer(serializers.ModelSerializer):
         validated_data["user"] = request.user
 
         return super().create(validated_data)
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = "__all__"
+        read_only_fields = ["cart"]
+
+    def create(self, validated_data):
+        request = self.context["request"]
+        validated_data["cart"] = request.user.cart
