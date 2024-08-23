e8d0ee7482cb9c1508fa96b20809e9a39384f881
arshia <arshiarezagholi1212@gmail.com>
2024-08-23 14:24:19 +0330

    added docstrings for custom permission in accounts app

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -2,7 +2,23 @@ from rest_framework.permissions import BasePermission
 
 
 class IsAuthenticatedAndActive(BasePermission):
+    """
+    Custom permission to only allow authenticated and active users to access the view.
+
+    - Only authenticated and active users are granted permission.
+    """
+
     def has_permission(self, request, view):
+        """
+        Check if the request has permission to perform the action.
+
+        Args:
+            request (Request): The HTTP request object.
+            view (View): The view object.
+
+        Returns:
+            bool: True if the user is authenticated and active, False otherwise.
+        """
         return bool(
             request.user and request.user.is_authenticated and request.user.is_active
         )
