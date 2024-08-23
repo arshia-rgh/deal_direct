07ee84a57bb7cb3a8e68a8f61321902103ee71d5
arshia <arshiarezagholi1212@gmail.com>
2024-08-23 18:25:52 +0330

    added CartItem viewset

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from cart.models import Cart
+from cart.models import Cart, CartItem
 from cart.permissions import IsOwner
-from cart.serializers import CartSerializer
+from cart.serializers import CartSerializer, CartItemSerializer
 from products.mixins import ListCacheMixin, ThrottleMixin
 
 
@@ -13,3 +13,11 @@ class CartViewSet(ListCacheMixin, ThrottleMixin, viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Cart.objects.filter(user=self.request.user)
+
+
+class CartItemViewSet(ListCacheMixin, ThrottleMixin, viewsets.ModelViewSet):
+    serializer_class = CartItemSerializer
+    permission_classes = (IsAuthenticated, IsOwner)
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart=self.request.user.cart)
