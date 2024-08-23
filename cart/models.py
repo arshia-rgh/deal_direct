6f3e33948530a1ea4f5691970bcfbb85df575bab
arshia <arshiarezagholi1212@gmail.com>
2024-08-23 14:41:26 +0330

    added Cart Model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+from utils.base_model import BaseModel
+from products.models import Product
+
+
+class Cart(BaseModel):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name="cart")
+    products = models.ManyToManyField(to=Product, through="CartItem")
