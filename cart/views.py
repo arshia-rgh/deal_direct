fb851f92992e0882f8e8bb9789fe4575a7a51a88
arshia <arshiarezagholi1212@gmail.com>
2024-08-23 20:30:10 +0330

    added docstrings for the cart views

--- cart/views.py
+++ cart/views.py
@@ -9,23 +9,58 @@ from products.mixins import ListCacheMixin, ThrottleMixin
 
 
 class CartCreateApiView(ThrottleMixin, CreateAPIView):
+    """
+    API view to create a new cart.
+
+    This view allows authenticated users to create a new cart.
+    It uses the `CartSerializer` to handle serialization and deserialization.
+    """
+
     serializer_class = CartSerializer
     permission_classes = (IsAuthenticated,)
 
 
 class CartRetrieveDestroyAPIView(ThrottleMixin, RetrieveDestroyAPIView):
+    """
+    API view to retrieve or destroy a cart.
+
+    This view allows authenticated users to retrieve or delete their own cart.
+    It uses the `CartSerializer` to handle serialization and deserialization.
+    """
+
     serializer_class = CartSerializer
     permission_classes = (IsAuthenticated, IsOwner)
 
     def get_object(self):
+        """
+        Retrieve the cart for the current user.
+
+        Returns:
+            Cart: The cart instance for the current user.
+        """
+
         return Cart.objects.get(user=self.request.user)
 
 
 class CartItemViewSet(ListCacheMixin, ThrottleMixin, viewsets.ModelViewSet):
+    """
+    ViewSet for managing cart items.
+
+    This view allows authenticated users to perform CRUD operations on their cart items.
+    It uses the `CartItemSerializer` to handle serialization and deserialization.
+    """
+
     cache_key = "cart_items_list"
     queryset = CartItem.objects
     serializer_class = CartItemSerializer
     permission_classes = (IsAuthenticated, IsOwner)
 
     def get_queryset(self):
+        """
+        Retrieve the cart items for the current user's cart.
+
+        Returns:
+            QuerySet: The queryset of cart items for the current user's cart.
+        """
+
         return CartItem.objects.filter(cart=self.request.user.cart)
