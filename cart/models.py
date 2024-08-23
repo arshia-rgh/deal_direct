6acf200ca71a787ce70dee1625c6d16b5a494cb5
arshia <arshiarezagholi1212@gmail.com>
2024-08-23 14:43:41 +0330

    added CartItem modle

--- cart/models.py
+++ cart/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 from accounts.models import User
-from utils.base_model import BaseModel
 from products.models import Product
+from utils.base_model import BaseModel
 
 
 class Cart(BaseModel):
     user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name="cart")
     products = models.ManyToManyField(to=Product, through="CartItem")
+
+
+class CartItem(BaseModel):
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(to=Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
