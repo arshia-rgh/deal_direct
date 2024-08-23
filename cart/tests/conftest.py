aa6158378130b2e5798b27e135e98b1f0a302cd2
arshia <arshiarezagholi1212@gmail.com>
2024-08-23 16:41:13 +0330

    added fixture (test_product)

--- cart/tests/conftest.py
+++ cart/tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
+
 from accounts.models import User
+from products.models import Product
 
 
 @pytest.fixture
@@ -8,4 +10,12 @@ def test_user():
         username="testuser",
         email="test@gmail.com",
         password="testpassword12",
+    )
+
+
+@pytest.fixture
+def test_product():
+    return Product.objects.create(
+        name="Test Product",
+        price=10.00,
     )
