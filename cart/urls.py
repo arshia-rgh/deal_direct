775b532316df3132be7b4827344756c745a9a1d3
arshia <arshiarezagholi1212@gmail.com>
2024-08-23 18:31:32 +0330

    registered CartItem viewset to the urls

--- cart/urls.py
+++ cart/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from cart.views import CartViewSet
+from cart.views import CartViewSet, CartItemViewSet
 
 router = DefaultRouter()
 
 router.register("carts", CartViewSet)
+router.register("cart-items", CartItemViewSet)
 
+app_name = "carts"
 urlpatterns = [
     path("", include(router.urls)),
 ]
