335223e904a5eead0f4c39120e43c8284e4bf624
arshia <arshiarezagholi1212@gmail.com>
2024-08-23 03:52:25 +0330

    added tests for TestProductModel

--- products/tests/test_models.py
+++ products/tests/test_models.py
@@ -1,6 +1,25 @@
 import pytest
 
+from products.models import Product
+
 
 @pytest.mark.django_db
 class TestProductModel:
-    pass
+    def test_production_creation(self, test_product):
+        assert test_product.name == "test product name"
+        assert Product.objects.filter(name="test product name").exists()
+
+    def test_product_creation_invalid_data(self):
+        with pytest.raises(Exception):
+            Product.objects.create(name="", description="", image="invalid image")
+
+    def test_product_update(self, test_product):
+        test_product.name = "test name 2"
+        test_product.save()
+
+        assert test_product.name == "test name 2"
+
+    def test_product_delete(self, test_product):
+        test_product.delete()
+
+        assert not Product.objects.filter(name="test product name").exists()
