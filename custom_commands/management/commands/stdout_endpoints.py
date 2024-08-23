5c52aa1d02e20a62729d2ddee746de7bade9a160
arshia <arshiarezagholi1212@gmail.com>
2024-08-23 21:48:09 +0330

    bug fixed and added print_patterns

--- custom_commands/management/commands/stdout_endpoints.py
+++ custom_commands/management/commands/stdout_endpoints.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.urls import get_resolver
+from django.urls import get_resolver, URLPattern, URLResolver
 
 
 class Command(BaseCommand):
@@ -8,10 +8,18 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         url_patterns = get_resolver().url_patterns
         self.stdout.write("# API Endpoints\n")
-        for pattern in url_patterns:
-            self.stdout.write(f"## {pattern.name}\n")
-            self.stdout.write(f"**Pattern:** `{pattern.pattern}`\n")
-            self.stdout.write(
-                f"**View:** `{pattern.callback.__module__}.{pattern.callback.__name__}`\n"
-            )
-            self.stdout.write("\n")
+        self._print_patterns(url_patterns)
+
+    def _print_patterns(self, patterns, prefix=""):
+        for pattern in patterns:
+            if isinstance(pattern, URLPattern):
+                self.stdout.write(f"## {pattern.name or 'Unnamed'}\n")
+                self.stdout.write(f"**Pattern:** `{prefix}{pattern.pattern}`\n")
+                self.stdout.write(
+                    f"**View:** `{pattern.callback.__module__}.{pattern.callback.__name__}`\n"
+                )
+                self.stdout.write("\n")
+            elif isinstance(pattern, URLResolver):
+                self._print_patterns(
+                    pattern.url_patterns, prefix + str(pattern.pattern)
+                )
