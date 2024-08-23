360ce5ad6a1b01e1af56c791e819b3421aae0039
arshia <arshiarezagholi1212@gmail.com>
2024-08-23 20:28:02 +0330

    added docstrings for the cart app serializers

--- cart/serializers.py
+++ cart/serializers.py
@@ -4,12 +4,31 @@ from cart.models import Cart, CartItem
 
 
 class CartSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Cart model.
+
+    This serializer handles the serialization and deserialization of Cart objects.
+    It includes all fields of the Cart model and sets the 'user' field as read-only.
+    """
+
     class Meta:
         model = Cart
         fields = "__all__"
         read_only_fields = ["user"]
 
     def create(self, validated_data):
+        """
+        Create a new Cart instance.
+
+        This method sets the 'user' field to the current user from the request context.
+
+        Args:
+            validated_data (dict): The validated data for creating the Cart instance.
+
+        Returns:
+            Cart: The created Cart instance.
+        """
+
         request = self.context["request"]
         validated_data["user"] = request.user
 
@@ -17,11 +36,30 @@ class CartSerializer(serializers.ModelSerializer):
 
 
 class CartItemSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the CartItem model.
+
+    This serializer handles the serialization and deserialization of CartItem objects.
+    It includes all fields of the CartItem model and sets the 'cart' field as read-only.
+    """
+
     class Meta:
         model = CartItem
         fields = "__all__"
         read_only_fields = ["cart"]
 
     def create(self, validated_data):
+        """
+        Create a new CartItem instance.
+
+        This method sets the 'cart' field to the current user's cart from the request context.
+
+        Args:
+            validated_data (dict): The validated data for creating the CartItem instance.
+
+        Returns:
+            CartItem: The created CartItem instance.
+        """
+
         request = self.context["request"]
         validated_data["cart"] = request.user.cart
