7bad26cd1f4c58b910440a173032338287d74812
arshia <arshiarezagholi1212@gmail.com>
2024-08-23 04:16:56 +0330

    added test_list

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -1,9 +1,25 @@
 import pytest
+from django.urls import reverse
+from model_bakery import baker
+
+from products.models import Product
 
 
 @pytest.mark.django_db
 class TestProductViewSet:
-    pass
+    @pytest.fixture
+    def multiple_products(self):
+        products = []
+        for _ in range(20):
+            product = baker.make(Product)
+            products.append(product)
+        return products
+
+    def test_list(self, api_client, multiple_products):
+        response = api_client.get(reverse("products:product-list"))
+
+        assert response.status_code == 200
+        assert len(response.data) == 20
 
 
 @pytest.mark.django_db
