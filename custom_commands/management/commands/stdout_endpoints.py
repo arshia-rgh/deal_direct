a3b328e3ce34d7a0107c526a59d9920d06cbf652
arshia <arshiarezagholi1212@gmail.com>
2024-08-23 21:49:21 +0330

    made new command to stdout only lical apps not others

--- custom_commands/management/commands/stdout_endpoints.py
+++ custom_commands/management/commands/stdout_endpoints.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.urls import get_resolver, URLPattern, URLResolver
+from django.conf import settings
 
 
 class Command(BaseCommand):
@@ -11,14 +12,21 @@ class Command(BaseCommand):
         self._print_patterns(url_patterns)
 
     def _print_patterns(self, patterns, prefix=""):
+        local_apps = [
+            app.split(".")[0]
+            for app in settings.INSTALLED_APPS
+            if not app.startswith("django.")
+        ]
         for pattern in patterns:
             if isinstance(pattern, URLPattern):
-                self.stdout.write(f"## {pattern.name or 'Unnamed'}\n")
-                self.stdout.write(f"**Pattern:** `{prefix}{pattern.pattern}`\n")
-                self.stdout.write(
-                    f"**View:** `{pattern.callback.__module__}.{pattern.callback.__name__}`\n"
-                )
-                self.stdout.write("\n")
+                view_module = pattern.callback.__module__.split(".")[0]
+                if view_module in local_apps:
+                    self.stdout.write(f"## {pattern.name or 'Unnamed'}\n")
+                    self.stdout.write(f"**Pattern:** `{prefix}{pattern.pattern}`\n")
+                    self.stdout.write(
+                        f"**View:** `{pattern.callback.__module__}.{pattern.callback.__name__}`\n"
+                    )
+                    self.stdout.write("\n")
             elif isinstance(pattern, URLResolver):
                 self._print_patterns(
                     pattern.url_patterns, prefix + str(pattern.pattern)
