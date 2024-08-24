ada9a631755fb413b4d8f962e13a94651e1e2c60
arshia <arshiarezagholi1212@gmail.com>
2024-08-24 21:50:30 +0330

    overrided the validate method of serializer to if the user tried to create more than one order for a cart doesnt get 500

--- orders/serializers.py
+++ orders/serializers.py
@@ -9,6 +9,13 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = ["cart", "status", "created"]
         read_only_fields = ["cart", "status", "created"]
 
+    def validate(self, data):
+        request = self.context["request"]
+        cart = request.user.cart
+
+        if Order.objects.filter(cart=cart).exists():
+            raise serializers.ValidationError("An order already exists for this cart.")
+
     def create(self, validated_data):
         request = self.context["request"]
 
