c2c0837a71a122933dfc859c13fc413314db6221
arshia <arshiarezagholi1212@gmail.com>
2024-08-24 21:41:48 +0330

    registered OrderCreateAPIView to the urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import OrderCreateAPIView
+
+urlpatterns = [
+    path("create/", OrderCreateAPIView.as_view(), name="create-order"),
+]
