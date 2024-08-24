64500ea3ebd3d6bb123dff0a0356a763787ca740
arshia <arshiarezagholi1212@gmail.com>
2024-08-24 12:49:08 +0330

    added test_delete_cart

--- cart/tests/test_views.py
+++ cart/tests/test_views.py
@@ -71,3 +71,14 @@ class TestCartRetrieveDestroyAPIView:
 
         assert response.status_code == 200
         assert response.data["id"] == 1
+
+    def test_delete_cart(self, api_client, test_user, test_cart):
+        test_user.is_active = True
+        test_user.save()
+
+        api_client.force_authenticate(test_user)
+
+        response = api_client.delete(reverse("carts:cart-detail", kwargs={"pk": 1}))
+
+        assert response.status_code == 204
+        assert not Cart.objects.filter(user=test_user).exists()
