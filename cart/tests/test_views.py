4dbf577ec61057bbe0a41659ba6894992b06c811
arshia <arshiarezagholi1212@gmail.com>
2024-08-24 11:36:50 +0330

    added test_creation with authenticated user

--- cart/tests/test_views.py
+++ cart/tests/test_views.py
@@ -1,7 +1,20 @@
 import pytest
+from django.urls import reverse
+
+from cart.models import Cart
 
 
 @pytest.mark.django_db
 class TestCartCreateAPIView:
-    def create_cart_authenticated_user(self):
-        pass
+    def test_create_cart_authenticated_user(self, api_client, test_user):
+        test_user.is_active = True
+        test_user.save()
+
+        api_client.force_authenticate(test_user)
+
+        response = api_client.post(reverse("carts:cart"))
+
+        assert response.status_code == 201
+        assert Cart.objects.all().count() == 1
+        assert response.data["products"] == []
+        assert response.data["user"] == test_user.id
