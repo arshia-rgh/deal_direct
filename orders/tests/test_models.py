68171b196a3c74b571c2e89d60a72eaba93b2504
arshia <arshiarezagholi1212@gmail.com>
2024-08-24 20:38:22 +0330

    added test for deleting an order obj

--- orders/tests/test_models.py
+++ orders/tests/test_models.py
@@ -33,3 +33,12 @@ class TestOrderModel:
         order.refresh_from_db()
 
         assert order.status == Order.OrderStatusChoices.completed
+
+    def test_delete_order(self, test_cart):
+        order = Order.objects.create(cart=test_cart)
+
+        assert len(Order.objects.all()) == 1
+
+        order.delete()
+
+        assert len(Order.objects.all()) == 0
