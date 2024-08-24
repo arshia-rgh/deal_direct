ef11f7c22940fbe98c7280f51337bdcf9e1d346f
arshia <arshiarezagholi1212@gmail.com>
2024-08-24 21:43:31 +0330

    added throttle mixin and permission for the Order create APIView

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,10 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from orders.serializers import OrderSerializer
+from products.mixins import ThrottleMixin
 
 
-class OrderCreateAPIView(CreateAPIView):
+class OrderCreateAPIView(ThrottleMixin, CreateAPIView):
     serializer_class = OrderSerializer
+    permission_classes = (IsAuthenticated,)
