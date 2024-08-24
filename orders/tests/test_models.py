053fb890df07e7498a26997be8962671e0902777
arshia <arshiarezagholi1212@gmail.com>
2024-08-24 20:32:49 +0330

    added test for order obj creation

--- orders/tests/test_models.py
+++ orders/tests/test_models.py
@@ -1,7 +1,17 @@
 import pytest
+
 from orders.models import Order
 
 
 @pytest.mark.django_db
 class TestOrderModel:
-    pass
+    def test_order_creation(self, test_cart):
+        order = Order.objects.create(cart=test_cart)
+
+        assert len(Order.objects.all()) == 1
+        assert order.user == test_cart.user
+        assert order.products == [item.product for item in test_cart.cartitem_set.all()]
+        assert order.status == Order.OrderStatusChoices.pending
+        assert order.total_price == sum(
+            item.product.price * item.quantity for item in test_cart.cartitem_set.all()
+        )
