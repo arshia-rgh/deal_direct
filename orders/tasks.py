8da149ac0cd85f8389c253a485a515a3f642b3d7
arshia <arshiarezagholi1212@gmail.com>
2024-08-25 12:00:52 +0330

    added celery task to delete carts and make the order completed after 7 days

--- orders/tasks.py
+++ orders/tasks.py
@@ -8,7 +8,10 @@ def delete_cart_after_7_days(order_id):
     try:
         order = Order.objects.get(id=order_id)
         cart = order.cart
+
+        order.cart = None
+        cart.delete()
+        order.status = Order.OrderStatusChoices.completed
+        order.save()
+    except Order.DoesNotExist():
         pass
-    except:
-        pass
-    # TODO implement later
