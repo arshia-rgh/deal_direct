f745cec74807995ebe9afe136bd9fa2fa797e7ef
arshia <arshiarezagholi1212@gmail.com>
2024-08-25 12:06:50 +0330

    added view for paying the order

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -6,6 +7,7 @@ from rest_framework.views import APIView
 from accounts.tasks import update_wallet_balance
 from orders.models import Order
 from orders.serializers import OrderSerializer
+from orders.tasks import delete_cart_after_7_days
 from products.mixins import ThrottleMixin
 
 
@@ -15,17 +17,34 @@ class OrderCreateAPIView(ThrottleMixin, CreateAPIView):
 
 
 class OrderPayAPIView(ThrottleMixin, APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request, *args, **kwargs):
         user = request.user
 
-        order = Order.objects.get(cart__user=user)
+        try:
+            order = Order.objects.get(cart__user=user)
+        except Order.DoesNotExist:
+            return Response(
+                {"error": "Order not found"}, status=status.HTTP_404_NOT_FOUND
+            )
 
-        if order and order.total_price <= user.wallet:
+        if order.total_price <= user.wallet:
             update_wallet_balance.delay(-order.total_price)
 
             order.status = Order.OrderStatusChoices.sending
             order.save()
 
-            # TODO implement this later
+            delete_cart_after_7_days.apply_async(
+                (order.id,), countdown=7 * 24 * 60 * 60
+            )
 
-            return Response({"message": "Payment was successful"})
+            return Response(
+                {
+                    "message": "Payment was successful, Your order will be delivered in 7 days"
+                }
+            )
+
+        return Response(
+            {"error": "Insufficient wallet balance"}, status=status.HTTP_400_BAD_REQUEST
+        )
