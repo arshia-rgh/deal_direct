d24eeb6a3798906e2c79929eb82b37b18131f6d8
arshia <arshiarezagholi1212@gmail.com>
2024-08-26 18:01:36 +0330

    added Product and Cart to the admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
