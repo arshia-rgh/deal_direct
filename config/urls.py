3560df1e42c455f3ec57fe21e7b7d7d0b1864ad6
arshia <arshiarezagholi1212@gmail.com>
2024-08-28 11:48:57 +0330

    added spectacular views to the urls file

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,4 +30,15 @@ urlpatterns = [
     path("products/", include("products.urls")),
     path("carts/", include("cart.urls")),
     path("orders/", include("orders.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swaagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
