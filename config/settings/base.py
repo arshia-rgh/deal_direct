615a170d4265a19771c6a78806bc4d1982b7b996
arshia <arshiarezagholi1212@gmail.com>
2024-09-02 14:30:08 +0330

    added loggin in settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -156,3 +156,30 @@ ZARINPAL = {
     "SANDBOX": True,
     "MERCHANT": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
 }
+
+# logg settings
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+        "file": {
+            "class": "logging.FileHandler",
+            "filename": "general.log",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["console", "file"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+        }
+    },
+    "formatters": {
+        "verbose": {
+            "format": "{asctime} ({levelname})- {name}- {message}",
+            "style": "{",
+        }
+    },
+}
