c876775af84bb3f1a353e1a11c72d221b26835c3
arshia <arshiarezagholi1212@gmail.com>
2024-09-03 21:35:53 +0330

    added docstrings for the custom middleware

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -4,7 +4,28 @@ from django.utils.deprecation import MiddlewareMixin
 
 
 class SessionTrackingMiddleware(MiddlewareMixin):
+    """
+    Middleware to track the last activity time of authenticated users.
+
+    This middleware updates the session data with the current timestamp
+    whenever an authenticated user makes a request.
+
+    Methods:
+        process_request(request):
+            Updates the session data with the current timestamp if the user is authenticated.
+    """
+
     def process_request(self, request):
+        """
+        Updates the session data with the current timestamp if the user is authenticated.
+
+        Args:
+            request (HttpRequest): The HTTP request object.
+
+        Raises:
+            Session.DoesNotExist: If the session does not exist.
+        """
+
         if request.user.is_authenticated:
             session_key = request.session.session_key
             session = Session.objects.get(session_key=session_key)
