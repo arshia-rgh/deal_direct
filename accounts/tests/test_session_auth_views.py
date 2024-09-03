8805f0aca2c3819379da81144455a8a2e9e123f4
arshia <arshiarezagholi1212@gmail.com>
2024-09-03 18:22:21 +0330

    implemented test_list_all_session_successfully

--- accounts/tests/test_session_auth_views.py
+++ accounts/tests/test_session_auth_views.py
@@ -1,6 +1,16 @@
 import pytest
+from django.urls import reverse
 
 
 @pytest.mark.django_db
 class TestSessionListView:
-    pass
+    def test_list_all_session_successfully(self, api_client, active_user):
+        api_client.force_authenticate(active_user)
+
+        response = api_client.get(reverse("accounts:session_list"))
+
+        assert response.status_code == 200
+        for session in response.data:
+            assert "session_key" in session
+            assert "expire_date" in session
+            assert "last_activity" in session
