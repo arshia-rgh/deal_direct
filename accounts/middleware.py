818bc69f9f4acb0869b002faf99323ad839bc4d1
arshia <arshiarezagholi1212@gmail.com>
2024-09-03 18:00:46 +0330

    added custom middle ware to add last_activity to the session_data for a session

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -1,5 +1,18 @@
+from django.contrib.sessions.models import Session
+from django.utils import timezone
 from django.utils.deprecation import MiddlewareMixin
 
 
 class SessionTrackingMiddleware(MiddlewareMixin):
-    pass
+    def process_request(self, request):
+        if request.user.is_authenticated:
+            session_key = request.session.session_key
+            session = Session.objects.get(sesssion_key=session_key)
+
+            session_data = session.get_decoded()
+
+            session_data["last_activity"] = timezone.now().isoformat()
+
+            session.session_data = Session.objects.encode(session_data)
+
+            session.save()
