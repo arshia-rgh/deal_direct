2b25ed0e473b497cacb027bfb4f87b563c35bbc9
arshia <arshiarezagholi1212@gmail.com>
2024-09-03 18:04:06 +0330

    added session auth to the rest framework settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -120,6 +120,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.ScopedRateThrottle",
