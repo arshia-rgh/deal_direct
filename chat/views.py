7421458f29851454a7b8734e7501e4314a6f732f
arshia <arshiarezagholi1212@gmail.com>
2024-09-04 19:30:09 +0330

    added viewset for chatroom

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from chat.models import ChatRoom
+from chat.serializers import ChatRoomSerializer
+from utils.mixins import ThrottleMixin, LoggingMixin, ListCacheMixin
+
+
+class ChatRoomViewSet(
+    ListCacheMixin, ThrottleMixin, LoggingMixin, viewsets.ModelViewSet
+):
+    cache_key = "chat_rooms_list"
+    queryset = ChatRoom.objects.all()
+    serializer_class = ChatRoomSerializer
+    permission_classes = (IsAuthenticated,)
