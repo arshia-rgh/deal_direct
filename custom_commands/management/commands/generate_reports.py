20df5ad75eb07baa972bbb3b7ecae3e386784526
arshia <arshiarezagholi1212@gmail.com>
2024-09-04 14:13:09 +0330

    added new command for generate and send to users them activity

---
+++ custom_commands/management/commands/generate_reports.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+from accounts.models import User
+from accounts.tasks import send_account_activity_report
+
+
+class Command(BaseCommand):
+    help = "Generate account activity reports and send them to users"
+
+    def handle(self, *args, **options):
+        users = User.objects.filter(is_active=True, receive_reports=True)
+
+        for user in users:
+            send_account_activity_report.delay(user.id)
+        self.stdout.write(
+            self.style.SUCCESS("Successfully sent account activity reports")
+        )
