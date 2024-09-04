6246717532edf29861b07aabc132ec8f62144788
arshia <arshiarezagholi1212@gmail.com>
2024-09-04 22:48:07 +0330

    added routing file

---
+++ chat/routing.py
@@ -0,0 +1,7 @@
+from django.urls import re_path
+
+from chat.consumers import ChatRoomConsumer
+
+websocket_urlpatterns = [
+    re_path(r"ws/chat/(?P<room_name>\w+)/$", ChatRoomConsumer.as_asgi()),
+]
