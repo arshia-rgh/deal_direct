0e30658602dd270eaaa45b2b10f75a2dc736cfcc
arshia <arshiarezagholi1212@gmail.com>
2024-09-04 19:08:09 +0330

    added ChatRoom model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+from utils.base_model import BaseModel
+
+
+class ChatRoom(BaseModel):
+    name = models.CharField(max_length=255, unique=True)
+    participants = models.ManyToManyField(to=User, related_name="rooms")
