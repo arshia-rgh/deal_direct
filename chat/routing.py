b1106bb69cb14f0af632a281a4c91bec676b4b5f
arshia <arshiarezagholi1212@gmail.com>
2024-09-04 22:56:21 +0330

    changed websocket routing

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,7 @@
-from django.urls import re_path
+from django.urls import path
 
 from chat.consumers import ChatRoomConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_name>\w+)/$", ChatRoomConsumer.as_asgi()),
+    path("ws/chat/<str:room_name>/", ChatRoomConsumer.as_asgi()),
 ]
