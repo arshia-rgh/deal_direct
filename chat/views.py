00dda982f1724c278a1b76bc76cd7b632a591a19
arshia <arshiarezagholi1212@gmail.com>
2024-09-04 19:36:43 +0330

    added perform create to add current user as a participants

--- chat/views.py
+++ chat/views.py
@@ -13,3 +13,10 @@ class ChatRoomViewSet(
     queryset = ChatRoom.objects.all()
     serializer_class = ChatRoomSerializer
     permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        chat_room = serializer.save()
+
+        chat_room.add_participants(self.request.user)
+
+        chat_room.save()
