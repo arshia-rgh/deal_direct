efc141d6c64ad54b4c6fe6ebe257afba34b91b0a
arshia <arshiarezagholi1212@gmail.com>
2024-09-04 19:10:49 +0330

    overrided the save logic

--- chat/models.py
+++ chat/models.py
@@ -7,3 +7,10 @@ from utils.base_model import BaseModel
 class ChatRoom(BaseModel):
     name = models.CharField(max_length=255, unique=True)
     participants = models.ManyToManyField(to=User, related_name="rooms")
+
+    def save(self, **kwargs):
+        if not self.name:
+            participant_names = [user.username for user in self.participants.all()]
+            self.name = "_".join(participant_names)
+
+        super().save(**kwargs)
