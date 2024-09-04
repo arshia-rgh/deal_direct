9839fe3adf31e2650fc54573f931406fff5e5d91
arshia <arshiarezagholi1212@gmail.com>
2024-09-04 22:53:12 +0330

    added app name for the chat urls file

--- chat/urls.py
+++ chat/urls.py
@@ -6,5 +6,5 @@ from chat.views import ChatRoomViewSet
 router = DefaultRouter()
 
 router.register("chat-rooms", ChatRoomViewSet)
-
+app_name = "chats"
 urlpatterns = [path("", include(router.urls))]
