f10486a8f55f00e3ba45bcbc608171f37d8ad617
arshia <arshiarezagholi1212@gmail.com>
2024-09-05 15:24:12 +0330

    added tests for access chat room

--- chat/tests/test_views.py
+++ chat/tests/test_views.py
@@ -1,7 +1,9 @@
 import pytest
 from django.urls import reverse
 
+from accounts.models import User
 from chat.models import ChatRoom
+from chat.tests.conftest import api_client
 
 
 @pytest.mark.django_db
@@ -69,4 +71,39 @@ class TestChatRoomViewSet:
 
 @pytest.mark.django_db
 class TestAccessChatRoomView:
-    pass
+    def test_access_chat_room_view_authenticated(
+        self, api_client, test_product, test_active_user
+    ):
+        chat_room = ChatRoom.objects.create(product=test_product)
+        chat_room.participants.add(test_active_user)
+        api_client.force_login(test_active_user)
+
+        response = api_client.get(
+            reverse("chats:access-chat", kwargs={"room_name": chat_room.name})
+        )
+
+        assert response.status_code == 200
+        assert "room_name" in response.context
+        assert response.context["room_name"] == chat_room.name
+
+    def test_access_chat_room_view_unauthenticated(self, api_client, test_product):
+        chat_room = ChatRoom.objects.create(product=test_product)
+
+        response = api_client.get(
+            reverse("chats:access-chat", kwargs={"room_name": chat_room.name})
+        )
+
+        assert response.status_code == 302  # Redirect to login page
+
+    def test_access_chat_room_view_not_participant(self, api_client, test_product):
+        chat_room = ChatRoom.objects.create(product=test_product)
+        non_participant_user = User.objects.create_user(
+            username="nonparticipant", password="testpass", is_active=True
+        )
+        api_client.force_login(non_participant_user)
+
+        response = api_client.get(
+            reverse("chats:access-chat", kwargs={"room_name": chat_room.name})
+        )
+
+        assert response.status_code == 403  # Forbidden
