38001977b927430a5fdbc1149b09d984085fe869
arshia <arshiarezagholi1212@gmail.com>
2024-09-05 13:47:51 +0330

    added test post to create new chat room

--- chat/tests/conftest.py
+++ chat/tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from model_bakery import baker
+from rest_framework.test import APIClient
 
+from accounts.models import User
 from products.models import Product
 
 
@@ -9,3 +11,18 @@ def test_product():
     product = baker.make(Product)
 
     return product
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def test_active_user():
+    return User.objects.create_user(
+        username="testuser",
+        email="test@gmail.com",
+        password="testpassword12",
+        is_active=True,
+    )
--- chat/tests/test_views.py
+++ chat/tests/test_views.py
@@ -1,6 +1,26 @@
 import pytest
+from django.urls import reverse
+
+from chat.models import ChatRoom
 
 
 @pytest.mark.django_db
 class TestChatRoomViewSet:
-    pass
+    def test_create_new_chat_room(self, api_client, test_product, test_active_user):
+        api_client.force_authenticate(test_active_user)
+
+        response = api_client.post(
+            reverse("chats:chatroom-list"), data={"product": test_product.id}
+        )
+
+        assert response.status_code == 201
+        assert ChatRoom.objects.all().count() == 1
+        assert (
+            ChatRoom.objects.get(product=test_product).name
+            == f"{test_product.name} - Seller: {test_product.uploaded_by.username}"
+        )
+
+        assert (
+            test_active_user
+            in ChatRoom.objects.get(product=test_product).participants.all()
+        )
