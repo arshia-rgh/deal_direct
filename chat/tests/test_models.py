dbf1c33023de536ac2f8127c8e574446050a6254
arshia <arshiarezagholi1212@gmail.com>
2024-09-05 12:35:11 +0330

    added test for create new instance of ChatRoom model

--- chat/tests/test_models.py
+++ chat/tests/test_models.py
@@ -1,7 +1,11 @@
 import pytest
+from chat.models import ChatRoom
 
 
 @pytest.mark.django_db
 class TestChatRoomModel:
-    def test_create_new_instance_successfully(self):
-        pass
+    def test_create_new_instance_successfully(self, test_product):
+        instance = ChatRoom.objects.create(product=test_product)
+
+        assert ChatRoom.objects.all().count() == 1
+        assert instance.name is not None
