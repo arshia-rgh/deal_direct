f2817d7ffc131ea6c92a217c2b828052dbd247ea
arshia <arshiarezagholi1212@gmail.com>
2024-09-07 18:59:37 +0330

    added uvicorn and changed entrypoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 python manage.py migrate
 
 # Create a superuser
-python manage.py createsuperuser --noinput
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL || true
 
 # Start the ASGI server
 uvicorn  config.asgi:application --host 0.0.0.0 --port 8000
