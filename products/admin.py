337e581dfc0c9ba2e9a7ab39cc50197f1d7299b7
arshia <arshiarezagholi1212@gmail.com>
2024-09-10 20:11:54 +0330

    added ProductAdmin

--- products/admin.py
+++ products/admin.py
@@ -5,7 +5,13 @@ from .models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "name", "price", "category_name", "created", "modified"]
+    readonly_fields = ["created", "modified"]
+
+    def category_name(self, obj):
+        return obj.category.name
+
+    category_name.short_description = "Category"
 
 
 @admin.register(Category)
