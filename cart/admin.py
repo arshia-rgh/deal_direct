5c2c3d6b6684ea67cd8ff14ad4c3410640a6c522
arshia <arshiarezagholi1212@gmail.com>
2024-09-10 18:20:07 +0330

    added inline for displaying items in the cart with details

--- cart/admin.py
+++ cart/admin.py
@@ -3,11 +3,29 @@ from django.contrib import admin
 from .models import Cart, CartItem
 
 
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 1
+    readonly_fields = ("product", "quantity", "price", "total")
+    can_delete = False
+
+    def price(self, obj):
+        return obj.product.price
+
+    price.short_description = "Price"
+
+    def total(self, obj):
+        return obj.product.price * obj.quantity
+
+    total.short_description = "Total"
+
+
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ["id", "user", "created", "modified", "total_price"]
     search_fields = ["user__username", "user__email"]
     list_filter = ["created", "modified"]
+    inlines = [CartItemInline]
 
     def total_price(self, obj):
         return sum(
