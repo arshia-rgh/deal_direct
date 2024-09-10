47b7c2ff09a13f8c67a477f2077913daa14d0192
arshia <arshiarezagholi1212@gmail.com>
2024-09-10 18:38:52 +0330

    added ChatRoomAdmin page more details

--- chat/admin.py
+++ chat/admin.py
@@ -5,4 +5,12 @@ from chat.models import ChatRoom
 
 @admin.register(ChatRoom)
 class ChatRoomAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "name", "product__name", "participants"]
+
+    def product__name(self, obj):
+        return obj.product.name
+
+    product__name.short_description = "Product Name"
+
+    def participants(self, obj):
+        return obj.participants.all()
