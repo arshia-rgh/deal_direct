108ea77a3e8217f41d56041fdee6a93fac711f48
arshia <arshiarezagholi1212@gmail.com>
2024-09-10 18:49:05 +0330

    added filter horizontal for ChatRoomAdmin and changed participants

--- chat/admin.py
+++ chat/admin.py
@@ -6,6 +6,7 @@ from chat.models import ChatRoom
 @admin.register(ChatRoom)
 class ChatRoomAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "product__name", "participants"]
+    filter_horizontal = ["participants"]
 
     def product__name(self, obj):
         return obj.product.name
@@ -13,4 +14,6 @@ class ChatRoomAdmin(admin.ModelAdmin):
     product__name.short_description = "Product Name"
 
     def participants(self, obj):
-        return obj.participants.all()
+        return ", ".join([user.username for user in obj.participants.all()])
+
+    participants.short_description = "Participants"
