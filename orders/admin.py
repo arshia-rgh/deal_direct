4060e3356815e26e50deecdb3e035cf5901c10fd
arshia <arshiarezagholi1212@gmail.com>
2024-09-10 19:00:26 +0330

    implemented OrderAdmin

--- orders/admin.py
+++ orders/admin.py
@@ -5,4 +5,9 @@ from .models import Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "user", "product_names", "total_price", "status"]
+
+    def product_names(self, obj):
+        return ", ".join([product.name for product in obj.products])
+
+    product_names.short_description = "Product Names"
