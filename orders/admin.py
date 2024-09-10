cbc1c9fc93a3b633c000b928c5756be5e46c2531
arshia <arshiarezagholi1212@gmail.com>
2024-09-10 19:06:16 +0330

    added more details for OrderAdmin

--- orders/admin.py
+++ orders/admin.py
@@ -5,7 +5,18 @@ from .models import Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ["id", "user", "product_names", "total_price", "status"]
+    list_display = [
+        "id",
+        "user",
+        "product_names",
+        "total_price",
+        "status",
+        "created",
+        "modified",
+    ]
+    readonly_fields = ["created", "modified"]
+    list_filter = ["created", "modified", "status"]
+    search_fields = ["cart__user__username", "cart__user__email"]
 
     def product_names(self, obj):
         return ", ".join([product.name for product in obj.products])
